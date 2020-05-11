a83f40c183ca3cd3659bdeba7ae157da5f511507
James Moreau <jamiemoreau@live.com>
2020-05-11 18:03:03 -0400

    fixed timings

--- renew.py
+++ renew.py
@@ -4,6 +4,7 @@ import os
 import wget
 import zipfile
 import urllib.request
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,8 +26,12 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://ocul-gue.primo.exlibrisgroup.com/discovery/login?vid=01OCUL_GUE:GUELPH")
 
+    time.sleep(5)
+
     # login type selection page
     driver.find_element_by_xpath('//*[@id="tab-content-0"]/div/md-content/md-list/md-list-item[1]/div/button').click()
+
+    time.sleep(5)
 
     # uofg login page
     username_entry = driver.find_element_by_xpath('//*[@id="inputUsername"]')
@@ -37,11 +42,17 @@ if __name__ == "__main__":
     password_entry.click()
     password_entry.send_keys(password)
 
+    time.sleep(5)
+
     driver.find_element_by_xpath('//*[@id="main"]/article/form/div[4]/div/button').click()
+    
+    time.sleep(5)
 
     #omni main page
     driver.get('https://ocul-gue.primo.exlibrisgroup.com/discovery/account?vid=01OCUL_GUE:GUELPH&section=overview&lang=en')
 
-    wait1 = WebDriverWait(driver, 10)
-    first_result = wait1.until(presence_of_element_located((By.CSS_SELECTOR, "#tab-content-1 > div > div > div > prm-loans-overview > div > div > div > div > button")))
-    first_result.click()
+    time.sleep(5)
+    
+    renew_button = driver.find_element_by_css_selector('#tab-content-0 > div > div > div > prm-loans-overview > div > div > div > div > button').click()
+    #first_result = wait1.until(presence_of_element_located((By.CSS_SELECTOR, "#tab-content-1 > div > div > div > prm-loans-overview > div > div > div > div > button")))
+    #first_result.click()
